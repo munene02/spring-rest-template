15f572a1f533b926dea9fe1a92d9e98015e21100
Martin Munene <munene02@gmail.com>
2023-11-13 17:29:58 +0300

    mapped response to a Map

--- src/main/java/com/martinmunene/springresttemplatedemo/client/BeerClientImpl.java
+++ src/main/java/com/martinmunene/springresttemplatedemo/client/BeerClientImpl.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 /**
  * @author Martin Munene
  */
@@ -16,12 +18,18 @@ import org.springframework.web.client.RestTemplate;
 public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
+    private static final String BASE_PATH = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
+
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
-        System.out.println(response.getBody());
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, String.class);
+
+        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, Map.class);
+
+        System.out.println(mapResponse.getBody());
 
         return null;
     }
